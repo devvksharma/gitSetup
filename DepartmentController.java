38e5d0649907fb2850f8b2b4a409fdacae57c7f4
Vijay <dev.vksharma@gmail.com>
2022-07-25 02:57:20 +0530

    first commit

---
+++ DepartmentController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.vijayks.controller;
+
+/**
+ * @author Lenovo
+ *
+ */
+public class DepartmentController {
+
+	/**
+	 * 
+	 */
+	public DepartmentController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
